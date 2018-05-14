468cf3d4d6849de59b48d879ac51a4349fa05023
SebouChu <somniia.99@gmail.com>
2018-05-14 16:28:07 +0200

    fix

--- lib/promethee/core_ext/tags.rb
+++ lib/promethee/core_ext/tags.rb
@@ -6,7 +6,8 @@ module ActionView
       # https://github.com/rails/rails/blob/bdc581616b760d1e2be3795c6f0f3ab4b1e125a5/actionview/lib/action_view/helpers/tags/text_field.rb
       class PrometheeEditor < Base
         def render
-          @options[:master_data] = object.send @method_name unless object.nil? || @options.key?(:value)
+          @options[:master_data] = object.send @method_name unless object.nil?
+          @options[:master_data] = @options[:value] if @options.key?(:value)
           ApplicationController.renderer.render partial: 'promethee/edit', locals: @options
         end
       end
