6e819498367879d4d57412daf573b52e92fad7b9
Arnaud Levy <alevy@lespoupees.paris>
2018-05-15 15:49:11 +0200

    remove test app

--- test/dummy/app/assets/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-//= require jquery
-//= require jquery-ui
-//= require jquery_ujs
-//= require bootstrap
-//= require angular
-//= require angular-animate
-//= require summernote/summernote
-//= require activestorage
-//= require promethee
