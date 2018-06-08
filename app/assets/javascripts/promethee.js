dedbb7f3eff630b67f17c7d319f09785249629e5
paboissinot <paboissinot@lespoupees.paris>
2018-06-08 17:34:42 +0200

    colorpicker

--- app/assets/javascripts/promethee.js
+++ app/assets/javascripts/promethee.js
@@ -1,3 +1,4 @@
 //= require angular-summernote/dist/angular-summernote
 //= require angular-ui-sortable/dist/sortable
 //= require ng-file-upload/dist/ng-file-upload
+//= require angular-bootstrap-colorpicker/js/bootstrap-colorpicker-module
