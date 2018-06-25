fda9d55db78b5886ad942b4efa28d7fae445d19c
paboissinot <paboissinot@lespoupees.paris>
2018-06-25 16:09:45 +0200

    lock unload pages

--- app/assets/javascripts/promethee.js
+++ app/assets/javascripts/promethee.js
@@ -4,3 +4,16 @@
 //= require angular-bootstrap-colorpicker/js/bootstrap-colorpicker-module
 //= require fancybox/dist/js/jquery.fancybox
 //= require promethee/fancybox
+
+window.onbeforeunload = function(evt) {
+  if (document.body.className.match(/\bpromethee-page-locked\b/)) {
+    var messageText = 'You have modified this page. If you quit this page without saving it, all changes will be lost.';
+    if (typeof evt == 'undefined') {
+        evt = window.event;
+    }
+    if (evt) {
+        evt.returnValue = messageText;
+    }
+    return messageText;
+  }
+};
