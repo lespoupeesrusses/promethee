2ba1a29e8795ca94080f0e0ee43527b90a31c1b7
paboissinot <paboissinot@lespoupees.paris>
2018-06-26 18:34:41 +0200

    renderer simplifié

--- lib/promethee/rails/helper.rb
+++ lib/promethee/rails/helper.rb
@@ -1,6 +1,6 @@
 module Promethee::Rails::Helper
   def promethee(data, options = {})
-    ApplicationController.renderer.render partial: 'promethee/show', locals: { master_data: data, localization_data: options[:l] }
+    render partial: 'promethee/show', locals: { master_data: data, localization_data: options[:l] }
   end
 
   def promethee_class_for component, *modifiers
--- lib/promethee/rails/version.rb
+++ lib/promethee/rails/version.rb
@@ -1,5 +1,5 @@
 module Promethee
   module Rails
-    VERSION = '1.6.37'
+    VERSION = '1.6.38'
   end
 end
