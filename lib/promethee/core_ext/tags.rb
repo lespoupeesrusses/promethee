346770c9d6dc1c098ea9a40fbac324d53343c7c7
Arnaud Levy <alevy@lespoupees.paris>
2018-07-26 09:31:55 +0200

    wip

--- lib/promethee/core_ext/tags.rb
+++ lib/promethee/core_ext/tags.rb
@@ -16,13 +16,13 @@ module ActionView
         def render
           localization_data = object.send @method_name unless object.nil?
           localization_data = @options[:value] if @options.include? :value
-          master_data = @options[:master]
           ApplicationController.renderer.render partial: 'promethee/localize',
                                                 locals: {
                                                   object_name: @object_name,
                                                   method_name: @method_name,
                                                   localization_data: localization_data,
-                                                  master_data: master_data
+                                                  master_data: @options[:master],
+                                                  other_data: @options[:other]
                                                 }
         end
       end
--- lib/promethee/data.rb
+++ lib/promethee/data.rb
@@ -4,6 +4,7 @@ module Promethee
     def initialize(data, options = {})
       @master_data = hashify data
       @localization_data_raw = hashify(options[:localization_data]) if options.include? :localization_data
+      @other_data = hashify(options[:other_data]) if options.include? :other_data
       check_master_integrity
       prepare_localization_data
       localize_master_data
@@ -51,17 +52,40 @@ module Promethee
         version: @master_data[:version],
         components: []
       }
-      # We create a flat list of the children and all their child, in the master's order
-      master_components_flat = Promethee::Data.flatten_components @master_data.deep_dup
-      master_components_flat.each do |component|
-        localized_component = Promethee::Data.find_localized_component component[:id], @localization_data_raw
-        # We take either the localized, or the master component
-        data = localized_component || component
+      master_components_flat.each do |master_component|
+        localized_component = find_localized_component master_component[:id]
+        # We take the localized component if it exists, the master component otherwise
+        data = localized_component || master_component
         # We add master reference
-        data[:master] = component.deep_dup
+        data[:master] = master_component.deep_dup
+        # We add localized component in other languages
+        data[:other] = add_other_data_for master_component[:id]
         # We add it to the list of localized components
         @localization_data[:components] << data
       end
+    end
+
+    def add_other_data_for(component_id)
+      hash = {}
+      return hash if @other_data.nil?
+      @other_data.each do |other_key, other_value|
+        localized_component = find_localized_component_in_array component_id, other_value
+        hash[other_key] = localized_component if localized_component
+      end
+      hash
+    end
+
+    # We create a flat list of the children and all their child, in the master's order
+    def master_components_flat
+      Promethee::Data.flatten_components @master_data.deep_dup
+    end
+
+    def find_localized_component(id)
+      find_localized_component_in_array id, @localization_data_raw
+    end
+
+    def find_localized_component_in_array(id, array)
+      Promethee::Data.find_localized_component id, array
     end
 
     # Recursively merge the localization_data into the master_data
