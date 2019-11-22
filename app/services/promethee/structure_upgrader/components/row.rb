592d2198c0d86ce0e82498de748464c9f3b70ced
Sébastien Gaya <sebastien.gaya@gmail.com>
2019-11-22 13:57:12 +0100

    row: handle backgrounds attribute

--- app/services/promethee/structure_upgrader/components/row.rb
+++ app/services/promethee/structure_upgrader/components/row.rb
@@ -24,7 +24,51 @@ module Promethee::StructureUpgrader::Components
           'searchable' => false,
           'translatable' => false,
           'type' => 'array',
-          'value' => attribute('backgrounds')
+          'value' => upgraded_backgrounds(attribute('backgrounds'))
+        }
+      }
+    end
+
+    private
+
+    def upgraded_backgrounds(backgrounds)
+      backgrounds.map { |background|
+        {
+          'image' => {
+            'searchable' => false,
+            'type' => 'blob',
+            'value' => background['image']
+          },
+          'posh' => {
+            'collection' => [
+              { 'label' => 'Left', 'value' => 'left' },
+              { 'label' => 'Center', 'value' => 'center' },
+              { 'label' => 'Right', 'value' => 'right' }
+            ],
+            'searchable' => false,
+            'type' => 'enum',
+            'value' => background['posh']
+          },
+          'posv' => {
+            'collection' => [
+              { 'label' => 'Top', 'value' => 'top' },
+              { 'label' => 'Center', 'value' => 'center' },
+              { 'label' => 'Bottom', 'value' => 'bottom' }
+            ],
+            'searchable' => false,
+            'type' => 'enum',
+            'value' => background['posv']
+          },
+          'size' => {
+            'collection' => [
+              { 'label' => 'Auto', 'value' => 'auto' },
+              { 'label' => 'Cover', 'value' => 'cover' },
+              { 'label' => 'Contain', 'value' => 'contain' }
+            ],
+            'searchable' => false,
+            'type' => 'enum',
+            'value' => background['size']
+          }
         }
       }
     end
